152d6c6af45faeba007bd1db6e8445fd7b21026c
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2022-04-27 14:34:04 -0700

    Delete OTRExporter directory

--- OTRExporter/OTRExporter/Main.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <Archive.h>
-
-extern std::shared_ptr<Ship::Archive> otrArchive;
-extern std::map<std::string, std::vector<char>> files;
--- OTRExporter/OTRExporter/VersionInfo.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <map>
-#include <vector>
-#include "Resource.h"
-
-#define MAJOR_VERSION Ship::Version::Deckard
-
-extern std::map<Ship::ResourceType, uint32_t> resourceVersions;
