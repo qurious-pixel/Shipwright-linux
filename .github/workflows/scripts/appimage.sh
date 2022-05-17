0f31b85ab3e5acb49d348d72cd1dc75ead0ba9fe
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2022-05-17 06:56:10 -0700

    set rpath for OTRGui

--- .github/workflows/scripts/appimage.sh
+++ .github/workflows/scripts/appimage.sh
@@ -26,7 +26,7 @@ chmod +x AppDir/usr/bin/assets/extractor/ZAPD.out
 cd AppDir && ln -s ./usr/bin/soh.sh ./AppRun && cd ..
 
 export UPD_INFO="gh-releases-zsync|qurious-pixel|Shipwright-linux|test|SOH-Linux.AppImage.zsync"
-ARCH=i386 ./linuxdeploy-x86_64.AppImage --appdir="$GITHUB_WORKSPACE"/AppDir/ -d "$GITHUB_WORKSPACE"/AppDir/soh.desktop -i "$GITHUB_WORKSPACE"/AppDir/soh.png -e "$GITHUB_WORKSPACE"/AppDir/usr/bin/soh.elf
+ARCH=i386 ./linuxdeploy-x86_64.AppImage --appdir="$GITHUB_WORKSPACE"/AppDir/ -d "$GITHUB_WORKSPACE"/AppDir/soh.desktop -i "$GITHUB_WORKSPACE"/AppDir/soh.png -e "$GITHUB_WORKSPACE"/AppDir/usr/bin/soh.elf -e "$GITHUB_WORKSPACE"/AppDir/usr/bin/OTRGui
  
 cp /lib/i386-linux-gnu/{ld-linux.so.2,libGL.so.1,libstdc++.so.6,libm.so.6,libgcc_s.so.1,libc.so.6,libxcb.so.1,libGLdispatch.so.0,libGLX.so.0} AppDir/usr/lib32
 cp /lib/i386-linux-gnu/{libpthread.so.0,libdl.so.2,libz.so.1,libXau.so.6,libXdmcp.so.6,libXext.so.6,librt.so.1,libbsd.so.0,libresolv.so.2,libgpg-error.so.0,libX11.so.6} AppDir/usr/lib32
