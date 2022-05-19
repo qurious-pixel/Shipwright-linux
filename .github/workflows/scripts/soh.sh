3a5c46f40d8070c613afd625771bfc788d647926
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2022-05-19 06:28:06 -0700

    Rearrange Env Vars

--- .github/workflows/scripts/soh.sh
+++ .github/workflows/scripts/soh.sh
@@ -1,32 +1,26 @@
 #!/bin/bash
+
 HERE="$(dirname "$(readlink -f "${0}")")"/../..
 
-export LD_LIBRARY_PATH="$HERE/usr/lib":$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH="$HERE/usr/lib32":$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH="$HERE/lib":$LD_LIBRARY_PATH
-
-#Sound Library
-export LD_LIBRARY_PATH="$HERE/usr/lib32/pulseaudio":$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH="$HERE/usr/lib32/alsa-lib":$LD_LIBRARY_PATH
-export LADSPA_PATH="$HERE/usr/lib32/ladspa":$LADSPA_PATH
-export AO_PLUGIN_PATH="$HERE/usr/lib32/ao/plugins-4":$AO_PLUGIN_PATH
-
-export PATH=$HERE/bin:$HERE/usr/bin:$PATH
-export LIBGL_DRIVERS_PATH="$HERE/usr/lib32/dri":$LIBGL_DRIVERS_PATH
-export MAIN32LDLIBRARY="$HERE/usr/lib32/ld-linux.so.2"
-
-if [[ ! -e $PWD/oot.otr ]]; then 
+if [[ ! -e $PWD/oot.otr ]]; then
 	export ASSETDIR=$(mktemp -d /tmp/assets-XXXXX)
-	ln -s $HERE/usr/bin/{assets,soh.elf,OTRGui} $ASSETDIR
+	ln -s $HERE/usr/bin/{assets,soh.elf} $ASSETDIR
+	cp $HERE/usr/bin/OTRGui $ASSETDIR
 	export OLDPWD=$PWD
 	ln -s $OLDPWD/*.*64 $ASSETDIR
 	cd $ASSETDIR
-	MAIN="$ASSETDIR/OTRGui"
-	"$MAIN32LDLIBRARY" "$MAIN"
+	$ASSETDIR/OTRGui
 	cp "$ASSETDIR"/oot.otr $OLDPWD
 	sleep 3
 	rm -r "$ASSETDIR"
 else
+	export LD_LIBRARY_PATH="$HERE/usr/lib":$LD_LIBRARY_PATH
+	export LD_LIBRARY_PATH="$HERE/usr/lib32":$LD_LIBRARY_PATH
+	export LD_LIBRARY_PATH="$HERE/lib":$LD_LIBRARY_PATH
+	export LD_LIBRARY_PATH="$HERE/usr/lib32/pulseaudio":$LD_LIBRARY_PATH
+	export PATH=$HERE/bin:$HERE/usr/bin:$PATH
+	export LIBGL_DRIVERS_PATH="$HERE/usr/lib32/dri":$LIBGL_DRIVERS_PATH
+	export MAIN32LDLIBRARY="$HERE/usr/lib32/ld-linux.so.2"
 	ln -s "$HERE/usr/bin/gamecontrollerdb.txt" $PWD
 	MAIN="$HERE/usr/bin/soh.elf"
 	"$MAIN32LDLIBRARY" "$MAIN"
