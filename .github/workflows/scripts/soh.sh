eafed5aeab51f19982af38bf6f2545bc48cd61c8
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2022-06-08 06:24:54 -0700

    Run SOH after creating oot.otr

--- .github/workflows/scripts/soh.sh
+++ .github/workflows/scripts/soh.sh
@@ -8,14 +8,12 @@ export LD_LIBRARY_PATH="$HERE/lib":$LD_LIBRARY_PATH
 #Sound Library
 export LD_LIBRARY_PATH="$HERE/usr/lib32/pulseaudio":$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH="$HERE/usr/lib32/alsa-lib":$LD_LIBRARY_PATH
-export LADSPA_PATH="$HERE/usr/lib32/ladspa":$LADSPA_PATH
-export AO_PLUGIN_PATH="$HERE/usr/lib32/ao/plugins-4":$AO_PLUGIN_PATH
 
 export PATH=$HERE/bin:$HERE/usr/bin:$PATH
 export LIBGL_DRIVERS_PATH="$HERE/usr/lib32/dri":$LIBGL_DRIVERS_PATH
 export MAIN32LDLIBRARY="$HERE/usr/lib32/ld-linux.so.2"
 
-if [[ ! -e $PWD/oot.otr ]]; then 
+while [[ ! -e $PWD/oot.otr ]]; do
 	export ASSETDIR=$(mktemp -d /tmp/assets-XXXXX)
 	ln -s $HERE/usr/bin/{assets,soh.elf,OTRGui} $ASSETDIR
 	export OLDPWD=$PWD
@@ -40,14 +38,15 @@ if [[ ! -e $PWD/oot.otr ]]; then
 		echo "Restart $APPIMAGE to play!" 
 		sleep 3
 		rm -r "$ASSETDIR"
+		break
 	else
 		echo -e "\nPlace ROM in this folder\n"
 		exit
 	fi
-else
+done	
+	cd $OWD
 	ln -s "$HERE/usr/bin/gamecontrollerdb.txt" $PWD
 	MAIN="$HERE/usr/bin/soh.elf"
 	"$MAIN32LDLIBRARY" "$MAIN"
 	unlink $PWD/gamecontrollerdb.txt
-fi
 exit
