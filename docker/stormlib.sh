8308a0b1aef2bd8832925575ee27158c0a859784
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2022-06-16 13:15:25 -0700

    combine SOH and StormLib builds

--- docker/stormlib.sh
+++ docker/stormlib.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-rm -rv StormLib/build
+rm -r StormLib/build
 /opt/devkitpro/portlibs/switch/bin/aarch64-none-elf-cmake -B StormLib/build -S StormLib
 make -C StormLib/build -j$(nproc)
 make install -C StormLib/build
 cp -v /usr/local/lib/libstorm.a /opt/devkitpro/portlibs/switch/lib/
 cp -v /usr/local/include/Storm* /opt/devkitpro/portlibs/switch/include/
+
+make -j$(nproc) OPTFLAGS=-O2 DEBUG=0
