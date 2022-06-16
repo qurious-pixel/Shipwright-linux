5defbd175eb9b17b54812e0ebdedc8f946bf2b5e
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2022-06-16 06:22:22 -0700

    Create stormlib.sh

---
+++ docker/stormlib.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+/opt/devkitpro/portlibs/switch/bin/aarch64-none-elf-cmake -B StormLib/build -S StormLib
+make -C StormLib/build -j$(nproc)
+make install -C StormLib/build
+cp /usr/local/lib/libstorm.a /opt/devkitpro/portlibs/switch/lib/
+cp /usr/local/include/Storm* /opt/devkitpro/portlibs/switch/include/
+
+cp -av /usr/local/lib/libSDL2* /lib/x86_64-linux-gnu/
