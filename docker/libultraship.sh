88a3b4cd29eaef977a5e30c5ab8bfb96f41f6791
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2022-06-16 12:25:25 -0700

    Update libultraship.sh

--- docker/libultraship.sh
+++ docker/libultraship.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+cp -av /usr/local/lib/libSDL2* /lib/x86_64-linux-gnu/
 git config --global --add safe.directory /soh
 make setup -C soh -j$(nproc) OPTFLAGS=-O2 DEBUG=0 CC="gcc" CXX="g++"
 
