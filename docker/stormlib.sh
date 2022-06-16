06e37992cefdd231cd8b9e287e36023dd3ff843f
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2022-06-16 07:29:45 -0700

    -D_POSIX_C_SOURCE=200809L for ftruncate

--- docker/stormlib.sh
+++ docker/stormlib.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-/opt/devkitpro/portlibs/switch/bin/aarch64-none-elf-cmake -B StormLib/build -S StormLib
+/opt/devkitpro/portlibs/switch/bin/aarch64-none-elf-cmake -B StormLib/build -S StormLib -D_POSIX_C_SOURCE=200809L
 make -C StormLib/build -j$(nproc)
 make install -C StormLib/build
 cp /usr/local/lib/libstorm.a /opt/devkitpro/portlibs/switch/lib/
