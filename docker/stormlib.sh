c25cbd970c6f39cda5a5ad317f46014f690bce4e
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2022-06-16 06:44:53 -0700

    set working dir from build file

--- docker/stormlib.sh
+++ docker/stormlib.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-/opt/devkitpro/portlibs/switch/bin/aarch64-none-elf-cmake -B /soh/StormLib/build -S /soh/StormLib
-make -C /soh/StormLib/build -j$(nproc)
-make install -C /soh/StormLib/build
+/opt/devkitpro/portlibs/switch/bin/aarch64-none-elf-cmake -B StormLib/build -S StormLib
+make -C StormLib/build -j$(nproc)
+make install -C StormLib/build
 cp /usr/local/lib/libstorm.a /opt/devkitpro/portlibs/switch/lib/
 cp /usr/local/include/Storm* /opt/devkitpro/portlibs/switch/include/
 
